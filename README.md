d52e28e3a6c6834b48abc42d6b284f6e1c2c0808
Austin Riendeau <austin@apriendeau.com>
2015-08-12 10:57:05 -0600

    [master]: fixes link and exposes box

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 [![GoDoc](https://godoc.org/github.com/apriendeau/crayons?status.svg)](https://godoc.org/github.com/apriendeau/crayons)
 
 There are alot of ANSI color libraries but none of them were useful for larger
-impletations. It is heavily modeled after [faith/color](https://github.com/faith/color)
+impletations. It is heavily modeled after [fatih/color](https://github.com/fatih/color)
 but exposes things slightly differently and adds a concept of grouping
 different styles.
 
--- box.go
+++ box.go
@@ -10,23 +10,23 @@ var (
 	ErrNilCrayon  = errors.New("Crayon cannot be a nil reference")
 )
 
-type box struct {
+type Box struct {
 	Crayons map[string]*Crayon
 }
 
 // NewBox creates a box of crayons and applys a default setting. If Base is
 // nil, it will default to white text on a black background.
-func NewBox(base *Crayon) *box {
+func NewBox(base *Crayon) *Box {
 	c := make(map[string]*Crayon)
 	if base == nil {
 		base = New(DefaultFg, DefaultBg)
 	}
 	c["base"] = base
-	return &box{Crayons: c}
+	return &Box{Crayons: c}
 }
 
 // Store adds a crayon to the box for later retrieval.
-func (b *box) Store(name string, crayon *Crayon) error {
+func (b *Box) Store(name string, crayon *Crayon) error {
 	if crayon == nil {
 		return ErrNilCrayon
 	}
@@ -39,7 +39,7 @@ func (b *box) Store(name string, crayon *Crayon) error {
 }
 
 // Pick retrives a crayon for your coloring pleasure.
-func (b *box) Pick(name string) *Crayon {
+func (b *Box) Pick(name string) *Crayon {
 	c, ok := b.Crayons[name]
 	if !ok {
 		return b.Crayons["base"]
@@ -48,7 +48,7 @@ func (b *box) Pick(name string) *Crayon {
 }
 
 // Remove destroys a crayon from your box.
-func (b *box) Remove(name string) error {
+func (b *Box) Remove(name string) error {
 	if name == "base" {
 		return ErrRemoveBase
 	}
@@ -57,7 +57,7 @@ func (b *box) Remove(name string) error {
 }
 
 // Names returns the names of all the stored crayons
-func (b *box) Names() []string {
+func (b *Box) Names() []string {
 	keys := make([]string, 0, len(b.Crayons))
 	for k := range b.Crayons {
 		keys = append(keys, k)
